26bcb4eea6df8e41c978aa03bdc858bf642296e2
Mavireck <mavireck@outlook.fr>
2019-08-28 23:57:17 +0200

    Fixed roundedRectangle

--- kssmObjectsLibrairy.py
+++ kssmObjectsLibrairy.py
@@ -21,58 +21,25 @@ def add_centeredText(obj,text,font,fill=0):
 	imgDraw.text((int(0.5*obj.w-0.5*text_w),int(0.5*obj.h-0.5*text_h)),text,font=font,fill=fill)
 	return kssm.pillowImgToScreenObject(img,obj.x,obj.y)
 
-def roundedRectangle(x,y,w,h,radius=10,fill=255,outline=50):
-	img = Image.new('L', (w+1,h+1), color=white)
-	rect = ImageDraw.Draw(img, 'L')
-	rect.rounded_rectangle([(0,0),(w,h)],radius,fill=fill,outline=outline)
-	return kssm.pillowImgToScreenObject(img2,x,y)
 
+def roundedCorner(radius, fill=255,outline=50):
+    """Draw a round corner"""
+    corner = Image.new('L', (radius, radius), white)
+    draw = ImageDraw.Draw(corner)
+    draw.pieslice((0, 0, radius * 2, radius * 2), 180, 270, fill=fill,outline=outline)
+    return corner
+ 
+def roundedRectangle(x,y,w,h, radius=20, fill=255,outline=50):
+    """Draw a rounded rectangle"""
+    rectangle = Image.new('L', (w,h), white)
+    draw = ImageDraw.Draw(rectangle)
+    draw.rectangle([(0,0),(w,h)],fill=fill,outline=outline)
+    draw.line([(radius,h-1),(w-radius,h-1)],fill=outline,width=1)
+    draw.line([(w-1,radius),(w-1,h-radius)],fill=outline,width=1)
+    corner = roundedCorner(radius, fill,outline)
+    rectangle.paste(corner, (0, 0))
+    rectangle.paste(corner.rotate(90), (0, h - radius)) # Rotate the corner and paste it
+    rectangle.paste(corner.rotate(180), (w - radius, h - radius))
+    rectangle.paste(corner.rotate(270), (w - radius, 0))
+    return kssm.pillowImgToScreenObject(rectangle,x,y)
 
-
-def rounded_rectangle_pil_helper(self,xy, corner_radius=10, fill=None, outline=None):
-	"""
-	Stolen from Welchel @ https://stackoverflow.com/questions/7787375/python-imaging-library-pil-drawing-rounded-rectangle-with-gradient/7788322#7788322
-	"""
-	upper_left_point = xy[0]
-	bottom_right_point = xy[1]
-	self.rectangle(
-		[
-			(upper_left_point[0], upper_left_point[1] + corner_radius),
-			(bottom_right_point[0], bottom_right_point[1] - corner_radius)
-		],
-		fill=fill,
-		outline=outline
-	)
-	self.rectangle(
-		[
-			(upper_left_point[0] + corner_radius, upper_left_point[1]),
-			(bottom_right_point[0] - corner_radius, bottom_right_point[1])
-		],
-		fill=fill,
-		outline=outline
-	)
-	self.pieslice([upper_left_point, (upper_left_point[0] + corner_radius * 2, upper_left_point[1] + corner_radius * 2)],
-		180,
-		270,
-		fill=fill,
-		outline=outline
-	)
-	self.pieslice([(bottom_right_point[0] - corner_radius * 2, bottom_right_point[1] - corner_radius * 2), bottom_right_point],
-		0,
-		90,
-		fill=fill,
-		outline=outline
-	)
-	self.pieslice([(upper_left_point[0], bottom_right_point[1] - corner_radius * 2), (upper_left_point[0] + corner_radius * 2, bottom_right_point[1])],
-		90,
-		180,
-		fill=fill,
-		outline=outline
-	)
-	self.pieslice([(bottom_right_point[0] - corner_radius * 2, upper_left_point[1]), (bottom_right_point[0], upper_left_point[1] + corner_radius * 2)],
-		270,
-		360,
-		fill=fill,
-		outline=outline
-	)
-ImageDraw.rounded_rectangle = rounded_rectangle_pil_helper
