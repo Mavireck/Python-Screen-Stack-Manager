eac5b9218cc6137dda92bcdaa20d339cb9f1ddd8
Mavireck <mavireck@outlook.fr>
2020-05-30 22:20:27 +0200

    Avoid empty region refresh

--- devices/kobo/device.py
+++ devices/kobo/device.py
@@ -115,7 +115,7 @@ def print_pil(imgData,x,y,w,h,length=None,isInverted=False):
 		fbink_cfg.is_nightmode = mode
 
 
-def do_screen_refresh(isInverted=False, isFlashing=True, isInvertionPermanent=True, area=[(0,0),(0,0)]):
+def do_screen_refresh(isInverted=False, isFlashing=True, isInvertionPermanent=True, area=[(0,0),(screen_width,screen_height)]):
 	initial_is_flashing = bool(fbink_cfg.is_flashing)
 	initial_is_nigthmode = bool(fbink_cfg.is_nightmode)
 	if isFlashing:
